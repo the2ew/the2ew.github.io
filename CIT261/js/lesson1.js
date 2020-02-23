c56836c53bbe90b41e8b44fb60302777100ffda7
the2ew <ctewthe2@yahoo.com>
2020-02-22 21:04:34 -0700

    Lesson#1-CIT261
    Almost finished creating JavaScript for lesson 1. Added structure to
    webpages and made the base javaScript to expand upon.

--- CIT261/js/lesson1.js
+++ CIT261/js/lesson1.js
@@ -1,12 +1,67 @@
-var x= 'Error';
+//var x= 'Error';
+//
+//var r = confirm("Are you sure you want to send a payment?");
+//
+//if(r == true) {
+//   x = 'Payment sent';
+//}
+//else {
+//   x = 'Payment cancelled';
+//}
+//
+//alert(x);
 
-var r = confirm("Are you sure you want to send a payment?");
+function smartForm() {
+  
+//Code to reformat containAns div id
+   document.getElementById('containAns').style.display ="block";
+   
+   
+   var rushmore = document.getElementById('rushmore').value.toLowerCase();
+//   var rushCompare = rushmore.toLowerCase();
+   var capitol = document.getElementById('capitol').value.toLowerCase();
+   
+   
+   if (document.getElementById('yes').checked){
+      
+       var radio = "yes";
+   } 
+   else if (document.getElementById('no').checked) {
+      
+      var radio = "no";
+   }
+   else {
+      
+      var radio ="Please select an answer";
+   }
+  
+   
+   var error = "";
+   var correct = "";
 
-if(r == true) {
-   x = 'Payment sent';
+   //Troubleshooting
+//   alert(rushmore);
+//   alert(capitol);
+    alert(radio);
+   
+   if (rushmore !== "washington") {
+     error = "The correct answer is Washington";
+     document.getElementById('answers').innerHTML = error;
+  } 
+   else {
+      correct = "You are correct!"
+      document.getElementById('answers').innerHTML = correct;
+   }
+ 
+   
 }
-else {
-   x = 'Payment cancelled';
+
+function clearDisplay(){
+   document.getElementById('containAns').style.display ="none";
 }
 
-alert(x);
+function showCode() {
+   var code = "x";
+   
+   document.getElementById("codeView").innerHTML = code;
+}
